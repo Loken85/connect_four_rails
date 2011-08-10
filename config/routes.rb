cae1778a7e7cfd840f60f6710ef19f3c803a91c0
Adrian Lindsay <Loken@.(none)>
2011-08-09 19:12:47 -0700

    Removed default rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ConnectFour::Application.routes.draw do
+
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+
+  root :to => 'pages#home'
+
+  get "users/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
