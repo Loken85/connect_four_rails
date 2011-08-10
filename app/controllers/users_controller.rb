9c3f1ff6ab90fca68a5701eb9c2cf89514c86d37
Adrian Lindsay <Loken@.(none)>
2011-08-09 20:57:05 -0700

    Users and Sessions are in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+   before_filter :authenticate, :only => [:edit, :update, :index, :destroy] #consider making index visible to admin only
+   before_filter :correct_user, :only => [:edit, :update]
+   before_filter :admin_user, :only => :destroy
+	
 
   def show
 	@user = User.find(params[:id])
@@ -21,5 +25,46 @@ class UsersController < ApplicationController
 	@user = User.new
 	@title = "Sign Up"
   end
+  
+  def edit	
+	@title = "Edit User"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit User"
+      render 'edit'
+    end
+  end
+  
+  def index
+	@title = "Users"
+	@users = User.paginate(:page => params[:page])
+  end
+  
+  def destroy
+	User.find(params[:id]).destroy
+	flash[:success] = "User Removed"
+	redirect_to users_path
+  end
+  
+  private
 
+    def authenticate
+      deny_access unless signed_in?
+    end
+	
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+	
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
+	end
+  
 end
